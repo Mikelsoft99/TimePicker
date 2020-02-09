e008ffbc359d3ff88be75e43a4cff5d37f210203
Michael Zimmerhofer <mikelsoft99@gmail.com>
2020-02-09 07:49:26 +0100

    compare with react-datepicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
+import "react-datepicker";
 import TimePicker from "./timePicker/timepicker";
 import "./styles.css";
+import "react-datepicker/dist/react-datepicker.css";
+
+import ReactDatePicker from "react-datepicker";
 
 export default function App() {
   return (
@@ -8,6 +12,15 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <TimePicker hour={4} minute={10} />
+      <div className="divider" />
+      <ReactDatePicker
+        onChange={e => alert(e)}
+        showTimeSelect
+        showTimeSelectOnly
+        selected={new Date()}
+        dateFormat="h:mm aa"
+        timeIntervals={5}
+      />
     </div>
   );
 }
